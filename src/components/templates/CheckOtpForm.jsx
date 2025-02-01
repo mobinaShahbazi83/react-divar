1600bcd81089502b28c6b62784cf612e69e2baf2
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-01 15:48:26 +0330

    Step two form to get otp

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,8 +1,19 @@
 
 
-function CheckOtpForm() {
+function CheckOtpForm({code,setCode, mobile, setStep}) {
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log({code, mobile})
+    }
   return (
-    <div>CheckOtpForm</div>
+  <form onSubmit={submitHandler}>
+    <p>تایید کد پیامک شده</p>
+    <span>کد پیامک شده به شماره "{mobile}" را وارد کنید</span>
+    <label htmlFor="input"> کد تایید را وارد کنید</label>
+    <input type="text" id="input" placeholder="کد تایید" value={code} onChange={e => setCode(e.target.value)}/> 
+    <button type="submit">ورود</button>
+    <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+  </form>
   )
 }
 
