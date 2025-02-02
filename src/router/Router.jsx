5aeebe814e686c7181c18cc5e6941e52da2c8891
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-02 18:58:28 +0330

    Create program routes

---
+++ src/router/Router.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import DashboardPage from "../pages/DashboardPage";
+import AuthPage from "../pages/AuthPage";
+import  PageNotFound from "../pages/404";
+import AdminPage from "../pages/AdminPage"
+
+function Router() {
+  return (
+    <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/dashboard" element={<DashboardPage/>}/>
+    <Route path="/" element={<AuthPage/>}/>
+    <Route path="/" element={<AdminPage/>}/>
+    <Route path="/" element={<PageNotFound/>}/>
+   </Routes>
+  )
+}
+
+export default Router
