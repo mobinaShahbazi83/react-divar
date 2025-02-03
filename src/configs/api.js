94a529e563e4ca2005885341a57bb8960413788f
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-03 22:14:40 +0330

    Getting a new access token by refresh token

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,19 +1,39 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import { getCookie, setCookie } from "../utils/cookie";
+import { getNewTokens } from "../services/token";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
-api.interceptors.request.use((request) => {
-  const accessToken = getCookie("accessToken");
-  if(accessToken) {
-    request.headers["Authorization"] = `bearer ${accessToken}`
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return request;
-}, error => {
-  return Promise.reject(error)
-});
+);
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const orginalRequest = error.config;
+    if (error.response.state === 401 && !orginalRequest._retry)
+      orginalRequest._retry = true;
+
+    const res = await getNewTokens();
+    if (!res?.response) return;
+    setCookie(res.response.data);
+
+    return api(orginalRequest);
+  }
+);
 export default api;
