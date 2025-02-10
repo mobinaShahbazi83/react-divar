7f3a2ba5c3b63b28f25a6675a1d5d1007071701e
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-10 13:02:59 +0330

    Styling the category list

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCategory } from "../../services/admin"
 import Loader from "../modules/Loader"
+import styles from "./CategoryList.module.css"
 
 
 function CategoryList() {
     const {data, isLoading} = useQuery(["get-categories"],getCategory )
     console.log({data, isLoading})
   return (
-    <div>
+    <div className={styles.list}>
         {isLoading ? <Loader/> : (data.data.map((i) =>
              (<div key={i._id}>
                 <img src={`${i.icon}.svg`}/>
