b78730cace74c652cc1c291074f2bc89669d14e5
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-14 13:45:27 +0330

    Show user ads in the user panel

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,11 +1,31 @@
 import { useQuery } from "@tanstack/react-query"
 import { getPost } from "../../services/user"
+import Loader from "../modules/Loader"
 
 
 function PostList() {
     const {data, isLoading} = useQuery(["my-post-list"], getPost)
   return (
-    <div></div>
+    <div>
+        {isLoading ? <Loader/> : (
+            <>
+            <h3>اگهی های شما</h3>
+            {data.data.posts.map((i) => (
+                <div key={post._id}><img src={`${import.meta.env.VITE_BASE_URL} ${post.images[0]}`}/>
+                <div>
+                    <p>{post.options.title}</p>
+                    <span>{post.options.content}</span>
+                </div>
+                <div>
+                    <p>{post.createdAt}</p>
+                    <span>{post.amount} تومان</span>
+                </div>
+                </div>
+            ))}
+            
+            </>
+        )} 
+    </div>
   )
 }
 
