81f1482a91bc48fe97f15b80ad7cc9e6fcc67acb
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-14 14:02:18 +0330

    Add css to the list of ads

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -2,22 +2,23 @@ import { useQuery } from "@tanstack/react-query"
 import { getPost } from "../../services/user"
 import Loader from "../modules/Loader"
 import { sp } from "../../utils/numbers"
+import styles from "./PostList.module.css"
 
 
 function PostList() {
     const {data, isLoading} = useQuery(["my-post-list"], getPost)
   return (
-    <div>
+    <div className={styles.list}>
         {isLoading ? <Loader/> : (
             <>
             <h3>اگهی های شما</h3>
             {data.data.posts.map((i) => (
-                <div key={post._id}><img src={`${import.meta.env.VITE_BASE_URL} ${post.images[0]}`}/>
+                <div key={post._id} className={styles.post}><img src={`${import.meta.env.VITE_BASE_URL} ${post.images[0]}`}/>
                 <div>
                     <p>{post.options.title}</p>
                     <span>{post.options.content}</span>
                 </div>
-                <div>
+                <div className={styles.price}>
                     <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
                     <span>{ sp(post.amount)} تومان</span>
                 </div>
