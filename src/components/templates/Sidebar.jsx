9233ba0681caa89f01eba055c21fb80c8b4cd138
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-02-16 20:23:59 +0330

    Sidebar and css development

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,8 +1,23 @@
+import { useQuery } from "@tanstack/react-query"
+import { getCategory } from "../../services/admin"
+import styles from "./Sidebar.module.css"
 
 
 function Sidebar() {
+    const {data} = useQuery(["get-categories"],getCategory)
+
   return (
-    <div>Sidebar</div>
+    <div className={styles.sidebar}>
+        <h4>دسته ها</h4>
+        <ul>
+            {data?.data.map((category) => (
+                <li key={category._id}>
+                    <img src={`${category.icon}.svg`}/>
+                    <p>{category.name}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
